1a78f1b89406a44c72769322863fc8aefcee1c42
Pete Field <pedalpete@hotmail.com>
2014-08-30 14:23:07 +1000

    fixed issue with missing dependencies

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,5 +1,7 @@
 var mock = require('./test-mock.json');
+var mock2 = require('./mocks/bower_list.json');
 var order_array = require('../lib/dependencyManager');
+var fs = require('fs');
 describe('order modules', function(){
     it('should get the mock as json', function(){
         expect(mock.endpoint.name).toBe('mock-bower-output');
@@ -16,4 +18,26 @@ describe('order modules', function(){
         expect(ordered_array[6]).toBe('lodash');
         expect(ordered_array[7]).toBe('restangular');
     });
+});
+
+
+
+describe('complete with missing modules', function(){
+    var new_array;
+      it('should not fail when dependencies are "missing"', function(){
+     
+    new_array = order_array(mock2);
+    
+     waitsFor(function() {
+        return new_array !== undefined;
+    }, 'should return a status that is not undefined', 431000);
+            
+      runs(function() {
+        expect(new_array[0]).toBe('jquery');
+        expect(new_array[39]).toBe('weather-icons');
+      });
+    });
+    
+ 
+    
 });
